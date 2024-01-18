1c300a3e4996b81e5a93558e25092fbb499f4309
Michał Nazarczuk <michaal94@gmail.com>
2024-01-18 19:10:20 +0000

    Update blender_install.sh

--- conda/blender_install.sh
+++ conda/blender_install.sh
@@ -4,4 +4,4 @@ sudo tar -xvf blender-2.83.20* --strip-components=1 -C /bin
 rm -rf blender-2.83.20*
 rm -rf blender-2.83.20*
 sudo rm -rf /bin/2.83/python
-sudo ln -s /home/$USER/anaconda3/envs/ns_ap /bin/2.83/python
+sudo ln -s /home/$USER/anaconda3/envs/clier /bin/2.83/python
